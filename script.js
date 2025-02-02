f718f80c585690063f1629b0bc40f84c24ae6a61
Tornike <chargeishvilitornike@gmail.com>
2025-02-02 15:00:15 +0400

    add errorMessage functionality

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const form = document.querySelector("form");
 const input = document.querySelector("input");
+const errorMessage = document.querySelector(".errorMessage");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     if (input.value === "") {
-        console.log("Whoops! It looks like you forgot to add your email");
+        errorMessage.innerText =
+            "Whoops! It looks like you forgot to add your email";
         input.style.borderColor = "hsl(354, 100%, 66%)";
     } else {
         const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         const isValid = regex.test(input.value);
 
         if (isValid) {
+            errorMessage.innerText = "";
             input.style.borderColor = "hsl(223, 100%, 88%)";
             input.style.value = "";
             setTimeout(() => {
@@ -21,7 +24,7 @@ form.addEventListener("submit", (e) => {
             });
         } else {
             input.style.borderColor = "hsl(354, 100%, 66%)";
-            console.log("Please provide a valid email address");
+            errorMessage.innerText = "Please provide a valid email address";
         }
     }
 });
